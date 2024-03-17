50021e1727d7e376fb5d0f7ee56a52058fc013ab
enzoenrico <e.enrico2005@gmail.com>
2024-03-17 17:20:42 -0300

    add: todo functional

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,58 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState<String[]>(["amongus", "boals"])
+  type todo = { id: string, todo: string, done: boolean }
+
+  const [todos, setTodos] = useState<todo[]>([
+    { id: '1', todo: 'amongus', done: false },
+    { id: '2', todo: 'boals', done: false }
+  ])
   const [todo, setTodo] = useState<string>('')
 
   const input = document.querySelector('input')
 
   const newTodo = () => {
-    if(input.value === ''){
+    if (input.value === '') {
       alert("Please enter a todo")
       return
-    } 
-    setTodos([...todos, todo])
+    }
+    const t = { id: Math.floor(Math.random() * 10).toString(), todo: todo, done: false }
+    setTodos([...todos, t])
     setTodo('')
     input.value = ''
+  }
+
+  const check = (e) => {
+    console.log(e.target.id)
+    setTodos(todos.map((t) => {
+      if (t.id === e.target.id) {
+        e.target.style.textDecoration = t.done ? 'none' : 'line-through'
+        return { ...t, done: !t.done }
+      } else {
+        return t
+      }
+    }))
   }
 
 
   return (
     <div className='main'>
-      <input type="text" name="" id="" onChange={(e) => setTodo(e.target.value)} />
+      <input type="text" onChange={(e) => setTodo(e.target.value)} />
       <button type="button" onClick={newTodo}></button>
 
       <table className='todos'>
         <td>
-          <td>Todo</td>
+          <td>Todos</td>
         </td>
         <tr>
-        {todos.map((e) => {
-          return <th className='todo'>
-            <input type="checkbox" name="done" id="" />
-            <p>{e}</p>
-          </th>
-        })}</tr>
+          {todos.map((e) => {
+            return (
+              <div id={e.id} className='todo' onClick={check}>
+                <p>{e.todo}</p>
+                {e.done ? 'done' : 'not done'}
+              </div>
+            )
+          })}</tr>
       </table>
     </div>
   )
